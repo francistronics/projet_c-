6a59c93da6d18d64c920dc5aee6a2ab33d84658a
Gahang Francis <francisgahang@yahoo.fr>
2014-04-25 15:03:20 +0200

    etape 2, trace bien les graphe

---
+++ DynamicData/DynamicData/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace DynamicData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnstop = new System.Windows.Forms.Button();
+            this.labeltemp = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.zedGraphControl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.zedGraphControl1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.zedGraphControl1.IsShowCursorValues = true;
+            this.zedGraphControl1.IsShowHScrollBar = true;
+            this.zedGraphControl1.IsShowVScrollBar = true;
+            this.zedGraphControl1.Location = new System.Drawing.Point(28, 12);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(722, 402);
+            this.zedGraphControl1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnstop
+            // 
+            this.btnstop.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnstop.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnstop.ForeColor = System.Drawing.Color.Red;
+            this.btnstop.Location = new System.Drawing.Point(205, 430);
+            this.btnstop.Name = "btnstop";
+            this.btnstop.Size = new System.Drawing.Size(564, 30);
+            this.btnstop.TabIndex = 1;
+            this.btnstop.Text = "stop";
+            this.btnstop.UseVisualStyleBackColor = true;
+            this.btnstop.Click += new System.EventHandler(this.btnstop_Click);
+            // 
+            // labeltemp
+            // 
+            this.labeltemp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labeltemp.AutoSize = true;
+            this.labeltemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labeltemp.Location = new System.Drawing.Point(756, 75);
+            this.labeltemp.Name = "labeltemp";
+            this.labeltemp.Size = new System.Drawing.Size(158, 55);
+            this.labeltemp.TabIndex = 2;
+            this.labeltemp.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(769, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Temperature en Celsius:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 471);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labeltemp);
+            this.Controls.Add(this.btnstop);
+            this.Controls.Add(this.zedGraphControl1);
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnstop;
+        private System.Windows.Forms.Label labeltemp;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DynamicData/DynamicData/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using ZedGraph;
+
+
+
+namespace DynamicData
+{
+    public partial class Form1 : Form
+    {
+        //mes rajouts///
+        UdpClient serveur = new UdpClient(9000);
+        UdpClient receivemessage = new UdpClient(13000);
+        byte[] datatosend = new byte[1024];
+        double val;
+        
+        ////////////////
+        // Starting time in milliseconds
+        int tickStart = 0;
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //////////////////////////////rajouts////////////////////////////////
+            
+            serveur.Connect(IPAddress.Parse("169.254.245.198"), int.Parse("8888"));
+            datatosend = ASCIIEncoding.ASCII.GetBytes("temperature");
+            serveur.Send(datatosend, datatosend.Length);
+            labeltemp.Text = "0";
+            
+            receivemessage.Client.ReceiveTimeout = 100;
+            receivemessage.Client.Blocking = false;
+            //////////////////////////////////////////////////////////////////////////////////////////
+            GraphPane myPane = zedGraphControl1.GraphPane;
+            myPane.Title.Text = "Test of Dynamic Data Update with ZedGraph\n" +
+                  "(After 25 seconds the graph scrolls)";
+            myPane.XAxis.Title.Text = "Time, Seconds";
+            myPane.YAxis.Title.Text = "temperature value";
+
+            // Save 1200 points.  At 50 ms sample rate, this is one minute
+            // The RollingPointPairList is an efficient storage class that always
+            // keeps a rolling set of point data without needing to shift any data values
+            RollingPointPairList list = new RollingPointPairList(1200);
+
+            // Initially, a curve is added with no data points (list is empty)
+            // Color is blue, and there will be no symbols
+            LineItem curve = myPane.AddCurve("temperature", list, Color.Gold, SymbolType.None);
+
+            // Sample at 50ms intervals
+            timer1.Interval = 50;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            // Just manually control the X axis range so it scrolls continuously
+            // instead of discrete step-sized jumps
+            myPane.XAxis.Scale.Min = 0;
+            myPane.XAxis.Scale.Max = 60;
+            myPane.XAxis.Scale.MinorStep = 1;
+            myPane.XAxis.Scale.MajorStep = 5;
+
+            // Scale the axes
+            zedGraphControl1.AxisChange();
+
+            // Save the beginning time for reference
+            tickStart = Environment.TickCount;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            /////rajouts////////////////////////
+            try
+            {
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 13000);
+                Byte[] rcvbytes = receivemessage.Receive(ref RemoteIpEndPoint);
+
+                val = double.Parse(ASCIIEncoding.ASCII.GetString(rcvbytes)); // on recupere la valeur recu pour lutiliser dans le tracer du graphe
+                labeltemp.Text = ASCIIEncoding.ASCII.GetString(rcvbytes);
+            }
+            catch (Exception ex)
+            {
+                string code = ex.HelpLink;
+            }
+            ////////////////////////////////////
+
+
+            // Make sure that the curvelist has at least one curve
+            if (zedGraphControl1.GraphPane.CurveList.Count <= 0)
+                return;
+
+            // Get the first CurveItem in the graph
+            LineItem curve = zedGraphControl1.GraphPane.CurveList[0] as LineItem;
+            if (curve == null)
+                return;
+
+            // Get the PointPairList
+            IPointListEdit list = curve.Points as IPointListEdit;
+            // If this is null, it means the reference at curve.Points does not
+            // support IPointListEdit, so we won't be able to modify it
+            if (list == null)
+                return;
+
+            // Time is measured in seconds
+            double time = (Environment.TickCount - tickStart) / 1000.0;
+
+            // 3 seconds per cycle
+            // list.Add(time, Math.Sin(2.0 * Math.PI * time / 3.0));
+
+
+            /////////rajout/////////////////////////////
+            list.Add(time, val);
+            ////////////////////////////////////////////
+
+
+
+            // Keep the X scale at a rolling 30 second interval, with one
+            // major step between the max X value and the end of the axis
+            Scale xScale = zedGraphControl1.GraphPane.XAxis.Scale;
+            if (time > xScale.Max - xScale.MajorStep)
+            {
+                xScale.Max = time + xScale.MajorStep;
+                xScale.Min = xScale.Max - 30.0;
+            }
+
+            // Make sure the Y axis is rescaled to accommodate actual data
+            zedGraphControl1.AxisChange();
+            // Force a redraw
+            zedGraphControl1.Invalidate();
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            SetSize();
+        }
+
+        // Set the size and location of the ZedGraphControl
+        private void SetSize()
+        {
+            // Control is always 100 pixels inset from the client rectangle of the form
+            Rectangle formRect = this.ClientRectangle;
+            formRect.Inflate(-100, -100);
+
+            if (zedGraphControl1.Size != formRect.Size)
+            {
+                zedGraphControl1.Location = formRect.Location;
+                zedGraphControl1.Size = formRect.Size;
+            }
+        }
+
+        private void btnstop_Click(object sender, EventArgs e)
+        {
+          //datatosend = ASCIIEncoding.ASCII.GetBytes(" ");
+         // serveur.Connect(IPAddress.Parse("169.254.245.198"), int.Parse("8888"));
+          datatosend = ASCIIEncoding.ASCII.GetBytes("stop");
+          serveur.Send(datatosend, datatosend.Length);
+          //zedGraphControl1.HorizontalScroll = false;
+        }
+        
+        }
+    }
+      
+    
+
